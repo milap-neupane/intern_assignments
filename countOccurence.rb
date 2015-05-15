3450e931e55a3faeee060777d4290bf1fd16b2eb
wannamit <wannamit@gmail.com>
2015-05-15 13:51:59 +0545

    Done with begin...rescue.

--- countOccurence.rb
+++ countOccurence.rb
@@ -1,13 +1,16 @@
 def CountPart(arr)
-  hs = Hash.new
+  hash = Hash.new
   arr.each do |value|
-    if hs.has_key?(value)
-      hs[value] += 1
-    else
-      hs[value] = 1
+    
+    begin
+      hash[value] += 1
+    rescue
+      hash[value] = 1
     end
+
   end
-  hs
+
+  hash
 end
 
 puts CountPart [2,4,6,3,6,7,4,1,4,8,1,5,7,9,0,1,0,4,5]
